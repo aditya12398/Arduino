4f097c05261c9b5e7100b5e62045d409ec776e7b
Aditya R. Prajapati <aditya12398@gmail.com>
2019-12-09 23:15:53 +0530

    Plot function snippet

--- Test_Display_ILI9486/UTM_Test.ino
+++ Test_Display_ILI9486/UTM_Test.ino
@@ -23,28 +23,37 @@ LiquidCrystal lcd(8, 9, 10, 11, 12, 13);
 
 int i, sign, clockpin_load = 17, clockpin_displacement = 4, datapin_load = 16, datapin_displacement = 3;
 long value;
-float load_result, displacement_result, c10=-128.8623, c4=-51.4, c1=-12.88623;
+float load_result, displacement_result, c10 = -128.8623, c4 = -51.4, c1 = -12.88623;
 
 unsigned long tempmicros;
 String timestamp, title, subtitle;
 File myFile;
 UTFT myGLCD(ILI9486, 38, 39, 40, 41);
 
-void setup() {
-  lcd.setCursor(0,0);
+void setup()
+{
+  //TFTLCD screen setup
+  randomSeed(analogRead(0));
+  myGLCD.InitLCD();
+  myGLCD.setFont(SmallFont);
+  //LCD Screen setup
+  lcd.setCursor(0, 0);
   Serial.begin(57600);
-  if (! rtc.begin()) {
+  if (!rtc.begin())
+  {
     lcd.print("Couldn't find RTC");
-    while (1);
+    while (1)
+      ;
   }
   Serial.begin(9600);
   pinMode(clockpin_load, INPUT);
   pinMode(datapin_load, INPUT);
   pinMode(clockpin_displacement, INPUT);
   pinMode(datapin_displacement, INPUT);
-  lcd.begin(20, 4);  // initializes the 20x4 LCD
-  lcd.setCursor(0,0);
-  if (! rtc.isrunning()) {
+  lcd.begin(20, 4); // initializes the 20x4 LCD
+  lcd.setCursor(0, 0);
+  if (!rtc.isrunning())
+  {
     lcd.print("RTC is NOT running!");
     // following line sets the RTC to the date & time this sketch was compiled
     rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
@@ -54,27 +63,29 @@ void setup() {
   }
 
   DateTime now = rtc.now();
-  subtitle =String(now.day(), DEC)+ String(now.month(), DEC)+ String(now.hour(), DEC) + String(now.minute(), DEC);
-  title =String(subtitle) + String(".csv");
+  subtitle = String(now.day(), DEC) + String(now.month(), DEC) + String(now.hour(), DEC) + String(now.minute(), DEC);
+  title = String(subtitle) + String(".csv");
   /*while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
   }*/
-  lcd.setCursor(0,1);
-  if (!SD.begin(53)) {
+  lcd.setCursor(0, 1);
+  if (!SD.begin(53))
+  {
     lcd.print("SD init. failed!");
     return;
   }
-  else{
-    timestamp = String(now.day(), DEC)+ String('/')+ String(now.month(), DEC) + String('/') + String(now.year(), DEC)+
-    String("|")+
-    String(now.hour(), DEC) + String(':')+ String(now.minute(), DEC) + String(':') + String(now.second(), DEC);
+  else
+  {
+    timestamp = String(now.day(), DEC) + String('/') + String(now.month(), DEC) + String('/') + String(now.year(), DEC) +
+                String("|") +
+                String(now.hour(), DEC) + String(':') + String(now.minute(), DEC) + String(':') + String(now.second(), DEC);
     myFile = SD.open(title, FILE_WRITE);
     lcd.print("SD initialized");
     myFile.print("File set up, Started at:, ");
     myFile.println(timestamp);
     myFile.println("Timestamp, 1 Ton Scale, 4 Ton Scale, 10 Ton Scale, Displacement");
   }
-  lcd.setCursor(0,2);
+  lcd.setCursor(0, 2);
   lcd.print("Filename:");
   lcd.print(String(title));
   myFile.close();
@@ -83,91 +94,128 @@ void setup() {
   lcd.clear();
 }
 
-void loop() {
+void loop()
+{
   myFile = SD.open(title, FILE_WRITE);
   DateTime now = rtc.now();
 
- while (digitalRead(clockpin_load)==HIGH) {} //if clock is LOW wait until it turns to HIGH
- tempmicros=micros();
- while (digitalRead(clockpin_load)==LOW) {} //wait for the end of the HIGH pulse
- if ((micros()-tempmicros)>500){ //if the HIGH pulse was longer than 500 micros we are at the start of a new bit sequence
-   decode1(); //decode1 the bit sequence
+  while (digitalRead(clockpin_load) == HIGH)
+  {
+  } //if clock is LOW wait until it turns to HIGH
+  tempmicros = micros();
+  while (digitalRead(clockpin_load) == LOW)
+  {
+  } //wait for the end of the HIGH pulse
+  if ((micros() - tempmicros) > 500)
+  {            //if the HIGH pulse was longer than 500 micros we are at the start of a new bit sequence
+    decode1(); //decode1 the bit sequence
   }
 
- while (digitalRead(clockpin_displacement)==HIGH) {} //if clock is LOW wait until it turns to HIGH
- tempmicros=micros();
- while (digitalRead(clockpin_displacement)==LOW) {} //wait for the end of the HIGH pulse
- if ((micros()-tempmicros)>500){ //if the HIGH pulse was longer than 500 micros we are at the start of a new bit sequence
-   decode2(); //decode2 the bit sequence
+  while (digitalRead(clockpin_displacement) == HIGH)
+  {
+  } //if clock is LOW wait until it turns to HIGH
+  tempmicros = micros();
+  while (digitalRead(clockpin_displacement) == LOW)
+  {
+  } //wait for the end of the HIGH pulse
+  if ((micros() - tempmicros) > 500)
+  {            //if the HIGH pulse was longer than 500 micros we are at the start of a new bit sequence
+    decode2(); //decode2 the bit sequence
   }
 
-  timestamp = String(now.day(), DEC)+ String('/')+ String(now.month(), DEC) + String('/') + String(now.year(), DEC)+
-  String("|")+
-  String(now.hour(), DEC) + String(':')+ String(now.minute(), DEC) + String(':') + String(now.second(), DEC);
+  timestamp = String(now.day(), DEC) + String('/') + String(now.month(), DEC) + String('/') + String(now.year(), DEC) +
+              String("|") +
+              String(now.hour(), DEC) + String(':') + String(now.minute(), DEC) + String(':') + String(now.second(), DEC);
 
   myFile.print(timestamp);
   myFile.print(", ");
-  myFile.print((load_result*c1), 2);
+  myFile.print((load_result * c1), 2);
   myFile.print(", ");
-  myFile.print((load_result*c4), 2);
+  myFile.print((load_result * c4), 2);
   myFile.print(", ");
-  myFile.print((load_result*c10), 2);
+  myFile.print((load_result * c10), 2);
   myFile.print(", ");
   myFile.println(displacement_result, 2);
-  lcd.setCursor(0,0);
+  lcd.setCursor(0, 0);
   lcd.print(timestamp);
-  lcd.setCursor(0,1);
+  lcd.setCursor(0, 1);
   lcd.print("1T:");
-  lcd.print((load_result*c1), 2);
+  lcd.print((load_result * c1), 2);
   lcd.print(" 4T:");
-  lcd.print((load_result*c4), 2);
-  lcd.setCursor(0,2);
+  lcd.print((load_result * c4), 2);
+  lcd.setCursor(0, 2);
   lcd.print("10T:");
-  lcd.print((load_result*c10), 2);
+  lcd.print((load_result * c10), 2);
   lcd.print(" D:");
   lcd.print((displacement_result), 2);
-  lcd.setCursor (0,3);
+  lcd.setCursor(0, 3);
   lcd.print("File: ");
   lcd.print(String(title));
   myFile.close();
   //delay(20);
 }
-void decode1() {
-  sign=1;
-  value=0;
-  for (i=0;i<23;i++) {
-    while (digitalRead(clockpin_load)==HIGH) { } //wait until clock returns to HIGH- the first bit is not needed
-    while (digitalRead(clockpin_load)==LOW) {} //wait until clock returns to LOW
-    if (digitalRead(datapin_load)==LOW) {
-      if (i<20) {
-        value|= 1<<i;
+void decode1()
+{
+  sign = 1;
+  value = 0;
+  for (i = 0; i < 23; i++)
+  {
+    while (digitalRead(clockpin_load) == HIGH)
+    {
+    } //wait until clock returns to HIGH- the first bit is not needed
+    while (digitalRead(clockpin_load) == LOW)
+    {
+    } //wait until clock returns to LOW
+    if (digitalRead(datapin_load) == LOW)
+    {
+      if (i < 20)
+      {
+        value |= 1 << i;
       }
-      if (i==20) {
-        sign=-1;
+      if (i == 20)
+      {
+        sign = -1;
       }
     }
   }
-  load_result=(value*sign)/100.00;
+  load_result = (value * sign) / 100.00;
 }
 
-void decode2() {
-  sign=1;
-  value=0;
-  for (i=0;i<23;i++) {
-    while (digitalRead(clockpin_displacement)==HIGH) { } //wait until clock returns to HIGH- the first bit is not needed
-    while (digitalRead(clockpin_displacement)==LOW) {} //wait until clock returns to LOW
-    if (digitalRead(datapin_displacement)==LOW) {
-      if (i<20) {
-        value|= 1<<i;
+void decode2()
+{
+  sign = 1;
+  value = 0;
+  for (i = 0; i < 23; i++)
+  {
+    while (digitalRead(clockpin_displacement) == HIGH)
+    {
+    } //wait until clock returns to HIGH- the first bit is not needed
+    while (digitalRead(clockpin_displacement) == LOW)
+    {
+    } //wait until clock returns to LOW
+    if (digitalRead(datapin_displacement) == LOW)
+    {
+      if (i < 20)
+      {
+        value |= 1 << i;
       }
-      if (i==20) {
-        sign=-1;
+      if (i == 20)
+      {
+        sign = -1;
       }
     }
   }
-  displacement_result=(value*sign)/100.00;
+  displacement_result = (value * sign) / 100.00;
 }
 
 void plot()
 {
+  double limit_load = c10 * 15;
+  double limit_disp = 15;
+  int max_width = 480;
+  int max_height = 320;
+  double x, y;
+  x = load_result / limit_load * max_width;
+  y = displacement_result / limit_disp * max_height;
+  myGLCD.drawPixel(5 + x, 5 + y);
 }
